09fd37871bbec35d3a5a93fbe5ac92d458a675cf
Jorubova Albina <deletawer@gmail.com>
2023-07-15 18:30:59 +0300

    changes

--- js/constructor.js
+++ js/constructor.js
@@ -2,7 +2,7 @@
 function createBlock() {
   const divBlock = document.createElement("div");
   divBlock.classList.add("divBlock");
-  divBlock.id = "TextBlock_TEXT_позиция_text";
+
   // Создаем новый блок p
   const P = document.createElement("P");
   divBlock.appendChild(P);
@@ -11,6 +11,7 @@ function createBlock() {
   var input = document.createElement("input");
   input.type = "text";
   input.classList.add("inputBtn");
+  input.name = "TextBlock_TEXT__позиция_text";
   var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Удалить";
   deleteButton.classList.add("dltBtn");
@@ -27,9 +28,9 @@ function createBlock() {
     P.innerHTML += text + "";
     input.value = "";
 
-    // Удаляем input и button
-    divBlock.removeChild(input);
-    divBlock.removeChild(button);
+    // Прячем input и button
+    input.style.display = "none";
+    button.style.display = "none";
   };
   divBlock.appendChild(input);
   divBlock.appendChild(button);
@@ -49,16 +50,15 @@ function createBlock_H1() {
   const H1 = document.createElement("h1");
   H1.classList.add("block");
   divBlock.appendChild(H1);
-
   // Создаем input для ввода текста
   var input = document.createElement("input");
   input.type = "text";
   input.classList.add("inputBtn");
+  input.name = "TextBlock_HEADERTEXT_текст_text";
   var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Удалить";
+  deleteButton.classList.add("dltBtn");
   deleteButton.onclick = function () {
-    // При клике на кнопку удаляем блок
-    // deleteButton.remove();
     divBlock.remove();
   };
 
@@ -71,9 +71,9 @@ function createBlock_H1() {
     H1.innerHTML += text + "";
     input.value = "";
 
-    // Удаляем input и button
-    divBlock.removeChild(input);
-    divBlock.removeChild(button);
+    // Прячем input и button
+    input.style.display = "none";
+    button.style.display = "none";
   };
   divBlock.appendChild(input);
   divBlock.appendChild(button);
@@ -87,11 +87,17 @@ function createBlock_H1() {
 function createBlock_TextBlock_Line() {
   // blockCount++; // Увеличиваем значение нарастающего числа id
 
+  const divBlock = document.createElement("div");
+  divBlock.classList.add("divBlock");
   // Создаем новый блок div
   const block = document.createElement("div");
 
   block.classList.add("designation"); // Добавляем класс "designation"
   block.classList.add("block");
+  divBlock.appendChild(block);
+  // Создаем элемент p и добавляем текст
+  const P = document.createElement("p");
+
   // Создаем SVG элемент
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("width", "10");
@@ -105,57 +111,91 @@ function createBlock_TextBlock_Line() {
   path.setAttribute("stroke", "white");
   path.setAttribute("stroke-width", "4");
   path.setAttribute("stroke-linecap", "round");
-
-  var deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Удалить";
-  deleteButton.onclick = function () {
-    // При клике на кнопку удаляем блок
-    deleteButton.remove();
-    block.remove();
-  };
-  // Добавляем path в svg
-  svg.appendChild(path);
-
-  // Создаем элемент p и добавляем текст
-  const text = document.createElement("p");
-  text.textContent = "текст";
-
   // Добавляем svg и p в блок
   block.appendChild(svg);
-  block.appendChild(text);
+  svg.appendChild(path);
+  block.appendChild(P);
+  // Создаем input для ввода текста
+  var input = document.createElement("input");
+  input.type = "text";
+  input.classList.add("inputBtn");
+  input.name = "TextBlock_QUOTETEXT_код_text";
+  var deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Удалить";
+  deleteButton.classList.add("dltBtn");
+  deleteButton.onclick = function () {
+    divBlock.remove();
+  };
+
+  // Создаем кнопку "добавить"
+  var button = document.createElement("button");
+  button.innerHTML = "Добавить";
+  button.onclick = function () {
+    // При клике на кнопку добавляем текст в параграф и очищаем поле ввода
+    var text = input.value;
+    P.innerHTML += text + "";
+    input.value = "";
+
+    // Прячем input и button
+    input.style.display = "none";
+    button.style.display = "none";
+  };
+  divBlock.appendChild(input);
+  divBlock.appendChild(button);
 
   // Добавляем новый блок в контейнер
   const container = document.getElementById("blocks-container");
-  container.appendChild(block);
-  container.appendChild(deleteButton);
+  container.appendChild(divBlock);
+  divBlock.appendChild(deleteButton);
 }
 
 //добавление блока с кодом
 function createBlock_TextBlock_CODE() {
   // blockCount++; // Увеличиваем значение нарастающего числа id
+  const divBlock = document.createElement("div");
+  divBlock.classList.add("divBlock");
 
+  // Создаем новый блок p
+  const P = document.createElement("P");
+  divBlock.appendChild(P);
+  P.classList.add("block");
   // Создаем новый блок div
   const block = document.createElement("div");
   block.classList.add("example_designation"); // Добавляем класс "designation"
   block.classList.add("block");
-  // Создаем элемент p и добавляем текст
-  const text = document.createElement("p");
-  text.textContent = 'текст = "Текст" ';
-
+  divBlock.appendChild(block);
+  block.appendChild(P);
+  // Создаем input для ввода текста
+  var input = document.createElement("input");
+  input.type = "text";
+  input.classList.add("inputBtn");
+  input.name = "TextBlock_CODE_code_text";
   var deleteButton = document.createElement("button");
   deleteButton.innerHTML = "Удалить";
+  deleteButton.classList.add("dltBtn");
   deleteButton.onclick = function () {
-    // При клике на кнопку удаляем блок
-    deleteButton.remove();
-    block.remove();
+    divBlock.remove();
   };
-  // Добавляем p в блок
-  block.appendChild(text);
 
+  // Создаем кнопку "добавить"
+  var button = document.createElement("button");
+  button.innerHTML = "Добавить";
+  button.onclick = function () {
+    // При клике на кнопку добавляем текст в параграф и очищаем поле ввода
+    var text = input.value;
+    P.innerHTML += text + "";
+    input.value = "";
+
+    // Прячем input и button
+    input.style.display = "none";
+    button.style.display = "none";
+  };
+  block.appendChild(input);
+  block.appendChild(button);
   // Добавляем новый блок в контейнер
   const container = document.getElementById("blocks-container");
-  container.appendChild(block);
-  container.appendChild(deleteButton);
+  container.appendChild(divBlock);
+  divBlock.appendChild(deleteButton);
 }
 
 //добавление блока с картинкой
@@ -206,136 +246,189 @@ function createBlock_hrBlock() {
   container.appendChild(deleteButton);
 }
 
-//текст + картинка
 function createBlock_ExplanationDesignation() {
-  // Создаем новый блок div с классом "explanation_designation"
   const explanationDesignation = document.createElement("div");
-  // explanationDesignation.id = "block-" + blockCount; // Присваиваем уникальный id
   explanationDesignation.classList.add("explanation_designation");
   explanationDesignation.classList.add("block");
-  //создаем input
-  // Создаем input для ввода текста
+
   var input = document.createElement("input");
   input.type = "text";
+  input.name = "CalloutBlock_позиция_text";
   input.classList.add("inputBtn");
 
-  // Создаем svg элемент
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("width", "22");
-  svg.setAttribute("height", "36");
-  svg.setAttribute("viewBox", "0 0 11 18");
-  svg.setAttribute("fill", "none");
+  var imageInput = document.createElement("input");
+  imageInput.type = "file";
+  imageInput.name = "CalloutBlock_карт_image";
+  imageInput.classList.add("inputBtn");
 
-  // Создаем path элементы и задаем атрибуты
-  const path1 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path1.setAttribute(
-    "d",
-    "M5.5 0C2.46766 0 0 2.43465 0 5.42666C0 6.54866 0.344668 7.62666 0.997345 8.54333C1.86999 9.77165 2.33199 11.1943 2.33199 12.65V12.6537H8.66801V12.65C8.66801 11.1943 9.13001 9.77165 10.0063 8.54333C10.6553 7.62666 11 6.54866 11 5.42666C11 2.43465 8.53234 0 5.5 0Z"
-  );
-  path1.setAttribute("fill", "#FFF598");
-
-  const path2 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path2.setAttribute("d", "M8.66755 13.387H2.33154V14.3367H8.66755V13.387Z");
-  path2.setAttribute("fill", "#FFF598");
-
-  const path3 = document.createElementNS("http://www.w3.org/2000/svg", "path");
-  path3.setAttribute(
-    "d",
-    "M2.33154 15.6383C2.33154 16.2543 2.83387 16.7567 3.45354 16.7567H3.95223C4.25654 17.27 4.85054 17.6 5.49955 17.6C6.14856 17.6 6.74255 17.27 7.04689 16.7567H7.54555C8.16522 16.7567 8.66755 16.2543 8.66755 15.6383V15.07H2.33154V15.6383Z"
-  );
-  path3.setAttribute("fill", "#FFF598");
-
-  // Добавляем path элементы в svg
-  svg.appendChild(path1);
-  svg.appendChild(path2);
-  svg.appendChild(path3);
-
-  // Создаем элемент p с текстом
+  const img = document.createElement("img");
   const p = document.createElement("p");
 
-  // Создаем кнопку "добавить"
   var button = document.createElement("button");
   button.innerHTML = "Добавить";
   button.onclick = function () {
-    // При клике на кнопку добавляем текст в параграф и очищаем поле ввода
     var text = input.value;
     p.innerHTML += text + "";
     input.value = "";
 
-    // Удаляем input и button
-    explanationDesignation.removeChild(input);
-    explanationDesignation.removeChild(button);
+    input.style.display = "none";
+    imageInput.style.display = "none";
+    button.style.display = "none";
   };
 
-  // Создаем кнопку "удалить"
+  imageInput.addEventListener("change", function (event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      img.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  });
+
   var deleteButton = document.createElement("button");
+  deleteButton.classList.add("dltBtn");
   deleteButton.innerHTML = "Удалить";
   deleteButton.onclick = function () {
-    // При клике на кнопку удаляем блок
     deleteButton.remove();
     explanationDesignation.remove();
-    // updateBlockIds();
   };
-
-  // Добавляем svg и p элементы в блок explanationDesignation
-  explanationDesignation.appendChild(svg);
+  explanationDesignation.appendChild(img);
+  // explanationDesignation.appendChild(svg);
   explanationDesignation.appendChild(p);
-  // Добавляем input и кнопку "добавить" к блоку
+
+  explanationDesignation.appendChild(imageInput);
   explanationDesignation.appendChild(input);
   explanationDesignation.appendChild(button);
-  // Добавляем новый блок в контейнер
+
   const container = document.getElementById("blocks-container");
   container.appendChild(explanationDesignation);
-  container.appendChild(deleteButton);
+  explanationDesignation.appendChild(deleteButton);
 }
 
-// //id
-// function assignIDs() {
-//   var blocks = document.getElementsByClassName("block");
-
-//   for (var i = 0; i < blocks.length; i++) {
-//     blocks[i].id = "block-" + (i + 1);
-//   }
-// }
-
-function dltBtn() {
+function hideBtn() {
   var buttons = document.getElementsByClassName("dltBtn");
 
-  while (buttons.length > 0) {
-    buttons[0].parentNode.removeChild(buttons[0]);
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].style.display = "none";
   }
 }
 
-function getPosition(divBlock) {
-  var parent = divBlock.parentNode;
-  var position = 0;
+// function saveInputs() {
+//   var inputs = document.querySelectorAll('input[type="text"]');
+//   var count = 1;
 
-  for (var i = 0; i < parent.children.length; i++) {
-    if (parent.children[i] == divBlock) {
-      position = i + 1;
-      break;
+//   inputs.forEach(function (input) {
+//     var name = input.name.split("_");
+//     var position = name[2];
+//     var type = name[1];
+//     var newName = "";
+
+//     if (type === "TEXT" && input.name.includes("_text")) {
+//       newName = "TextBlock_TEXT_" + count + "_text";
+//     } else if (type === "CODE" && input.name.includes("_text")) {
+//       newName = "TextBlock_CODE_" + count + "_text";
+//     } else if (type === "CalloutBlock" && input.name.includes("_text")) {
+//       newName = "CalloutBlock_" + count + "_text";
+//     } else if (type === "CalloutBlock" && input.name.includes("_image")) {
+//       newName = "CalloutBlock_" + count + "_image";
+//     } else if (type === "TextBlock" && input.name.includes("_HeaderText_")) {
+//       newName = "TextBlock_HEADERTEXT_" + count + "_text";
+//     } else if (type === "LinkBlock" && input.name.includes("_text")) {
+//       newName = "LinkBlock_" + count + "_text";
+//     } else if (type === "ImageBlock" && input.name.includes("_image")) {
+//       newName = "ImageBlock_" + count + "_image";
+//     } else if (type === "TextBlock" && input.name.includes("_QuoteText_")) {
+//       newName = "TextBlock_QUOTETEXT_" + count + "_text";
+//     } else if (type === "DividerBlock" && input.name.includes("_")) {
+//       newName = "DividerBlock_" + count;
+//     }
+
+//     input.name = newName;
+//     count++;
+//   });
+// }
+
+function saveInputs() {
+  var inputs = document.querySelectorAll('input[type="text"]');
+  var count = 1;
+
+  inputs.forEach(function (input) {
+    var name = input.name.split("_");
+    var type = name[0];
+    var newName = "";
+
+    if (type === "TextBlock" && input.name.includes("_позиция_text")) {
+      newName = "TextBlock_TEXT_" + count + "_text";
+    } else if (type === "TextBlock" && input.name.includes("_code_text")) {
+      newName = "TextBlock_CODE_" + count + "_text";
+    } else if (
+      type === "CalloutBlock" &&
+      input.name.includes("_позиция_text")
+    ) {
+      newName = "CalloutBlock_" + count + "_text";
+    } else if (type === "CalloutBlock" && input.name.includes("_карт_image")) {
+      newName = "CalloutBlock_" + count + "_image";
+    } else if (type === "TextBlock" && input.name.includes("_текст_text")) {
+      newName = "TextBlock_HEADERTEXT_" + count + "_text";
+    } else if (type === "LinkBlock" && input.name.includes("_позиция_text")) {
+      newName = "LinkBlock_" + count + "_text";
+    } else if (type === "ImageBlock" && input.name.includes("_позиция_image")) {
+      newName = "ImageBlock_" + count + "_image";
+    } else if (type === "TextBlock" && input.name.includes("_код_text")) {
+      newName = "TextBlock_QUOTETEXT_" + count + "_text";
+    } else if (type === "DividerBlock" && input.name.includes("_позиция")) {
+      newName = "DividerBlock_" + count;
     }
-  }
 
-  return position;
+    input.name = newName;
+    count++;
+  });
 }
 
+//id
 function assignIDs() {
-  var blocks = document.getElementsByClassName("blocks_container");
+  var blocks = document.getElementsByClassName("divBlock");
+  var input = document.getElementsByClassName("inputBtn");
 
   for (var i = 0; i < blocks.length; i++) {
-    var position = getPosition(blocks[i]); // Получаем место блока в общем блоке
-    var id = "";
-
-    if (blocks[i].classList.contains("TextBlock")) {
-      id = "TextBlock_TEXT_" + position + "_text";
-    } else if (blocks[i].classList.contains("CodeBlock")) {
-      id = "TextBlock_CODE_" + position + "_text";
-    } else if (blocks[i].classList.contains("LinkBlock")) {
-      id = "LinkBlock_" + position + "_text";
-    }
-
-    blocks[i].id = id;
+    blocks[i].id = "block-" + (i + 1);
   }
-  dltBtn();
+  saveInputs();
+  hideBtn();
 }
+
+// function getPosition(divBlock) {
+//   var parent = divBlock.parentNode;
+//   var position = 0;
+
+//   for (var i = 0; i < parent.children.length; i++) {
+//     if (parent.children[i] == divBlock) {
+//       position = i + 1;
+//       break;
+//     }
+//   }
+
+//   return position;
+// }
+
+// function assignIDs() {
+//   var blocks = document.getElementsByClassName("blocks_container");
+
+//   for (var i = 0; i < blocks.length; i++) {
+//     var position = getPosition(blocks[i]); // Получаем место блока в общем блоке
+//     var id = "";
+
+//     if (blocks[i].classList.contains("TextBlock")) {
+//       id = "TextBlock_TEXT_" + position + "_text";
+//     } else if (blocks[i].classList.contains("CodeBlock")) {
+//       id = "TextBlock_CODE_" + position + "_text";
+//     } else if (blocks[i].classList.contains("LinkBlock")) {
+//       id = "LinkBlock_" + position + "_text";
+//     }
+
+//     blocks[i].id = id;
+//   }
+//   dltBtn();
+// }
