56bf1ec8ec8679f981b9afd217bd52b621d5e4c7
Jorubova Albina <deletawer@gmail.com>
2023-07-21 13:25:49 +0300

    adapt_signIn_form

--- js/enter.js
+++ js/enter.js
@@ -2,9 +2,10 @@ const wrapper = document.querySelector(".wrapper");
 const mainInfo = document.querySelector(".mainInfo");
 const signInBtn = document.querySelector(".enter");
 const footer = document.querySelector("footer");
-
+const header = document.querySelector("header");
 signInBtn.addEventListener("click", () => {
   wrapper.classList.add("active");
+  header.classList.add("activet");
   mainInfo.classList.add("noactive");
   footer.classList.add("noactive");
 });
